cb16977992bbcb5b988ec556b9c0c266d6ed6468
koichi <satoyskkh200426@gmail.com>
2024-03-14 23:10:56 +0900

    ルーティング
    ルーティング

--- RubyonRails/BulletinBoardApp/config/routes.rb
+++ RubyonRails/BulletinBoardApp/config/routes.rb
@@ -1,10 +1,13 @@
+# このファイルではルーティングを設定することができる。
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # localhost:3000/postsのルーティングを設定する
+  # 以下のルーティングにより
+  # localhost:3000/posts/index
+  # localhost:3000/posts/new
+  # localhost:3000/posts/create
+  # のルーティングが完了した
+  resources :posts, only: [:index, :new, :create]
+  
 end
