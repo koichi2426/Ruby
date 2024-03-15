948bb944bd7cbaafcba00e49bae1cd890bf95b63
koichi <satoyskkh200426@gmail.com>
2024-03-15 15:15:46 +0900

    フォーム作成
    フォーム作成

--- RubyonRails/BulletinBoardApp/app/controllers/posts_controller.rb
+++ RubyonRails/BulletinBoardApp/app/controllers/posts_controller.rb
@@ -5,7 +5,20 @@
 # はPostsControllerがApplicationControllerを継承しているという意味
 class PostsController < ApplicationController
 
+    # ＜注意＞
+    # defの横に定義する名前はvuews/posts/[名前].html.erb/
+    # で対応しなくてはならない
+
+    # 例えば、def indexならindex.html.erbとなる
+
     # indexにアクセスが来た時の動作の設定
     def index  
+        # posts
+        @posts = Post.all
+    end
+
+    # newにアクセスが来た時の動作の設定
+    def new
+        @posts = Post.new
     end
 end
